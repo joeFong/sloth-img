739b76e1cf5304ed7f51268b39b6e8b94d38c92a
Joe Fong <joefong@github.com>
2020-01-21 16:32:00 -0800

    update old repo name to new

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,8 @@
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/sloth-img.js',
   mode: 'production',
-  optimization: {
-    minimizer: [new UglifyJsPlugin()],
-  },
   output: {
     filename: 'sloth-img.js',
     path: path.resolve(__dirname, 'public')
